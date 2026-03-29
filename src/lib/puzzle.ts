f1b7a1becbdd17b205ca6d8b5e11d6d2ddca9543
Kavinda Sumanaweera <kavindasu@gmail.com>
2026-03-29 04:53:03 +0000

    Minor Update

---
+++ src/lib/puzzle.ts
@@ -0,0 +1,14 @@
+export const regionColors: Record<number, string> = {
+  0: "bg-purple-300",
+  1: "bg-purple-200",
+  2: "bg-orange-200",
+  3: "bg-lime-200",
+  4: "bg-blue-300",
+  5: "bg-green-300",
+  6: "bg-yellow-200",
+  7: "bg-sky-300",
+  8: "bg-red-300",
+  9: "bg-lime-300",
+  10: "bg-pink-300",
+  11: "bg-stone-300",
+};
